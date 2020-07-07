c896534eb21cd50b9e58f05f3c7bb064b6aa564b
강현제 <hyunje2316@gmail.com>
2020-07-07 17:54:33 +0900

    0707 화 security-context.xml 생성 작업 미완료

---
+++ team_cafe/src/main/java/com/cafe/erp/cs/service/ShaEncoder.java
@@ -0,0 +1,20 @@
+package com.cafe.erp.cs.service;
+
+import javax.annotation.Resource;
+
+import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShaEncoder {
+	
+	@Resource(name="passwordEncoder")
+	ShaPasswordEncoder encoder;
+	
+	public String encoding(String str) {
+		return encoder.encodePassword(str, null);
+	}
+	public String saltEncoding(String str, String salt) {
+		return encoder.encodePassword(str, salt);
+	}
+}
---
+++ team_cafe/src/main/java/com/cafe/erp/cs/service/UserAuthenticationService.java
@@ -0,0 +1,25 @@
+package com.cafe.erp.cs.service;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserAuthenticationService implements UserDetailsService {
+
+	private SqlSessionTemplate sqlSession;
+	
+	public UserAuthenticationService() {
+	}
+
+	public UserAuthenticationService(SqlSessionTemplate sqlSession) {
+		this.sqlSession = sqlSession;
+	}
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
